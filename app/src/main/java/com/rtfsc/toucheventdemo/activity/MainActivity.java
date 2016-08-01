fad32e57bf795f8f2517cbeb56e6edc9a295d453
wongkimhung <kdroid@163.com>
2016-08-01 18:10:38 +0800

    update

--- app/src/main/java/com/rtfsc/toucheventdemo/activity/MainActivity.java
+++ app/src/main/java/com/rtfsc/toucheventdemo/activity/MainActivity.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.rtfsc.toucheventdemo.R;
 
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
 		Button button1 = (Button) findViewById(R.id.button1);
 		Button button2 = (Button) findViewById(R.id.button2);
+		Button button3 = (Button) findViewById(R.id.button3);
 
 		assert button1 != null;
 		button1.setOnClickListener(new View.OnClickListener() {
@@ -38,5 +44,14 @@ public class MainActivity extends AppCompatActivity {
 				startActivity(new Intent(MainActivity.this, InsideControllActivity.class));
 			}
 		});
+
+		assert button3 != null;
+		button3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, StickyLayoutActivity.class));
+			}
+		});
+
 	}
 }
